fbd602ff7bc24c84f8f6df008d37221edf0ccd7e
Navaneeth Rajagopalan <navaneethnvnthr@gmail.com>
2019-12-03 01:10:17 +1100

    Remove POC folder content
    Signed-off-by: Navaneeth Rajagopalan <navaneethnvnthr@gmail.com>

--- POC/Relative_Ranking.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-users = pd.read_csv('https://data.stackexchange.com/stackoverflow/csv/9320?Location=Melbourne')
-reputation = 1973
-reputations = users['Reputation']
-total_reputations = len(reputations)
-higher_reputations = (reputations > 1973).sum()
-print(higher_reputations / total_reputations)
--- POC/dummy.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-data = pd.read_csv('https://data.stackexchange.com/stackoverflow/csv/9320?Location=Melbourne')
-data.to_csv("Test.csv", sep='\t', encoding='utf-8')
