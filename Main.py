b24cdb7198866013b381d085a83a1a88190b3117
Navaneeth Rajagopalan <navaneethnvnthr@gmail.com>
2019-12-03 00:38:28 +1100

    Add function to pass SO URL and Regions from Command Line
    Signed-off-by: Navaneeth Rajagopalan <navaneethnvnthr@gmail.com>

--- Main.py
+++ Main.py
@@ -2,21 +2,18 @@ import sys
 sys.path.append('./User')
 from User import User
 
-#try:
-me = User('https://stackoverflow.com/users/4833507/Navaneeth')
-me.build_profile_info(['Melbourne', 'Sydney', 'Bengaluru'])
-print(me.user_name + " has a reputation of " + "{0:,}".format(me.reputation))
-if me.relative_ranking:
-    print(me.user_name + "'s relative ranking:")
-    for region in me.relative_ranking:
-        print("\ttop " + str(me.relative_ranking[region]) + "% in the region " + region)
+args = sys.argv
+so_url = args[1]
+regions = args[2:]
 
-dkularni = User('https://stackoverflow.com/users/696257/dkulkarni')
-dkularni.build_profile_info(['Melbourne', 'Sydney', 'Bengaluru'])
-print(dkularni.user_name + " has a reputation of " + "{0:,}".format(dkularni.reputation))
-if dkularni.relative_ranking:
-    print(dkularni.user_name + "'s relative ranking:")
-    for region in dkularni.relative_ranking:
-        print("\ttop " + str(dkularni.relative_ranking[region]) + "% in the region " + region)
-# except Exception as e:
-#     print(str(e))
+try:
+    so_user = User(so_url)
+    so_user.build_profile_info(regions)
+    print(so_user.user_name + " has a reputation of " + "{0:,}".format(so_user.reputation))
+    if so_user.relative_ranking:
+        print(so_user.user_name + "'s relative ranking:")
+        for region in so_user.relative_ranking:
+            print("\tTop " + str(so_user.relative_ranking[region]) + "% in the region " + region)
+
+except Exception as e:
+    print(str(e))
